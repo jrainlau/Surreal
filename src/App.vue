b07c2dd15e74968ef81e5d088c9a9eae7c9ab453
jrainliu <jrainliu@tencent.com>
2023-04-18 23:05:02 +0800

    feat: init basic functions

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- <canvas id="gradient-canvas"></canvas> -->
+</template>
+
+<script>
+import './core/gradient'
+</script>
+
+<style>
+#gradient-canvas {
+  width:100%;
+  height:100%;
+  --gradient-color-1: #fff; 
+  --gradient-color-2: #000; 
+  --gradient-color-3: #7038ff;  
+  --gradient-color-4: #ffba27;
+  z-index:0;
+}
+</style>
